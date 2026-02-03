cad8fe34eff044602b56d026db1ab629f7c55d24
Isaac Burgos <isaacburgos543@gmail.com>
2026-02-02 19:08:18 -0500

    feature:Agregando Develop

---
+++ bitacora-aprendizaje/src/main/java/dosw/bitacora/App.java
@@ -0,0 +1,13 @@
+package dosw.bitacora;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
