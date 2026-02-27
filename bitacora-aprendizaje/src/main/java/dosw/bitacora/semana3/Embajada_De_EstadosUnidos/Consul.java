32a0773a180ad45c513e8f5a62534f41f6a115ad
Isaac Burgos <isaacburgos543@gmail.com>
2026-02-27 07:41:09 -0500

    feature:Agregando ejercicio Embajada estados unidos semana 3

---
+++ bitacora-aprendizaje/src/main/java/dosw/bitacora/semana3/Embajada_De_EstadosUnidos/Consul.java
@@ -0,0 +1,13 @@
+package dosw.bitacora.semana3.Embajada_De_EstadosUnidos;
+
+public class Consul extends FuncionarioEmbajada {
+    @Override
+    public void procesarTramite(Tramite tramite) {
+        if (tramite.getTipo().equals("Visa")) {
+            System.out.println("Cónsul: Realizando entrevista y aprobando visa para: " + tramite.getDescripcion());
+        } else if (sucesor != null) {
+            System.out.println("Cónsul: Trámite fuera de mi jurisdicción. Escalando al Embajador...");
+            sucesor.procesarTramite(tramite);
+        }
+    }
+}
